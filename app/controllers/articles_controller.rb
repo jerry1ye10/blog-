d76226bfca2bc39056e786c7103b8efb5abcc806
jerry1ye10 <jerry1ye10@gmail.com>
2017-08-06 18:05:56 -0400

    eqw
    Added Comments feature and cleaned up code to make it neater

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,9 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  def index
+    @articles = Article.all
+  end
   def create
     @article = Article.new(params.require(:article).permit(:title, :text))
 
@@ -14,11 +17,11 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
   def edit
     @article = Article.find(params[:id])
     end
-
-def update
+  def update
   @article = Article.find(params[:id])
 
   if @article.update(params.require(:article).permit(:title, :text))
@@ -26,10 +29,13 @@ def update
   else
     render 'edit'
   end
-end
-
-def index
-    @articles = Article.all
-end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+end
 
