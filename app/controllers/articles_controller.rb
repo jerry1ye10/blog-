cfa5cb1d2a579cd58e496607581560dcef3e56f3
jerry1ye10 <jerry1ye10@gmail.com>
2017-08-06 16:18:55 -0400

    Pushes blog into repository

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,35 @@
+class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :text))
+
+    if @article.save
+    redirect_to @article
+    else
+      render 'new'
+    end
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
+  def edit
+    @article = Article.find(params[:id])
+    end
+
+def update
+  @article = Article.find(params[:id])
+
+  if @article.update(params.require(:article).permit(:title, :text))
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+
+def index
+    @articles = Article.all
+end
+  end
+
